67a1563bdf706176cfcd800b8072f0b34a9df004
Aleksandr <a.fedorov.work@gmail.com>
2021-07-31 08:49:22 +0300

    Async url requests

--- moex-connector.py
+++ moex-connector.py
@@ -1,19 +1,37 @@
+#!/usr/bin/env python
+import random
 import urllib.request
 import json
+import asyncio
 
-
-opener = urllib.request.build_opener(urllib.request.HTTPHandler())
-urllib.request.install_opener(opener)
 req = 'http://iss.moex.com/iss/history/engines/%(engine)s/markets/%(market)s/boards/%(board)s/securities.json?date=%(date)s'
-params = {
-    'engine': 'stock',
-    'market': 'bonds',
-    'board': 'TQOD',
-    'date': '20210728'
-}
-url = req % params
-print("url", url)
-res = opener.open(url + '&start=0')
-jres = json.load(res)
-for data in jres['history']['data']:
-    print(data)
+
+
+async def run(date: int) -> None:
+    global results
+
+    opener = urllib.request.build_opener(urllib.request.HTTPHandler())
+    urllib.request.install_opener(opener)
+    params = {
+        'engine': 'stock',
+        'market': 'bonds',
+        'board': 'TQOD',
+        'date': str(date)
+    }
+    url = req % params
+    print("url", url)
+    res = opener.open(url + '&start=0')
+    jres = json.load(res)
+    # for data in jres['history']['data']:
+    #     print(data)
+    await asyncio.sleep(random.randint(1, 5))
+    print(f"{date} finished")
+
+
+async def main():
+    await asyncio.gather(
+        *[run(date) for date in range(20210720, 20210730)]
+    )
+
+
+asyncio.run(main())
